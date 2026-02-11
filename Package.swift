d2b3ec713f58d40122a0bd89b30912eee5e357b7
pradhapbarani <baranipradhap@gmail.com>
2026-02-11 16:20:36 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,21 +1,22 @@
+// swift-tools-version: 5.7
 import PackageDescription
 
 let package = Package(
     name: "Shimmer",
     platforms: [
-        .iOS(.v11) // pick what your app supports; SPM/Xcode no longer targets iOS 6-era anyway
+        .iOS(.v11)
     ],
     products: [
-        .library(
-            name: "Shimmer",
-            targets: ["Shimmer"]
-        )
+        .library(name: "Shimmer", targets: ["Shimmer"])
     ],
     targets: [
         .target(
             name: "Shimmer",
-            path: "Sources/Shimmer",
-            publicHeadersPath: "include"
+            path: "FBShimmering",
+            publicHeadersPath: "include",
+            exclude: [
+                "Info.plist"
+            ]
         )
     ]
 )
